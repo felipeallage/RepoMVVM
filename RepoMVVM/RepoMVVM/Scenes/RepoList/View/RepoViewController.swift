5bc8701f310a4e2b6a6ed8f9c431140d4aba3677
Felipe Allage <felipeallage@hotmail.com>
2022-02-23 16:06:26 -0300

    .

--- RepoMVVM/RepoMVVM/Scenes/RepoList/View/RepoViewController.swift
+++ RepoMVVM/RepoMVVM/Scenes/RepoList/View/RepoViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class RepoViewController: UIViewController {
+    
+    
+    @IBOutlet weak var repoTableView: UITableView!
+    
+    var viewModel = RepoViewModel()
+    
+    var repoList: [Repository] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        repoTableView.register(RepoTableViewCell.self, forCellReuseIdentifier: RepoTableViewCell.identifier)
+        repoTableView.dataSource = self
+        repoTableView.delegate = self
+        viewModel.delegate = self
+        viewModel.repoBinding()
     }
 
 
@@ -20,11 +31,34 @@ class RepoViewController: UIViewController {
 extension RepoViewController: RepoViewModelDelegate {
     
     func didGetRepo(repo: [Repository]) {
-        <#code#>
+        self.repoList = repo
+        repoTableView.reloadData()
     }
     
-    
+}
+
+extension RepoViewController: UITableViewDelegate {
     
     
 }
 
+extension RepoViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        repoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = repoTableView.dequeueReusableCell(withIdentifier: RepoTableViewCell.identifier) as! RepoTableViewCell
+        let repository = repoList[indexPath.row]
+        cell.update(model: repository)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    
+    
+}
